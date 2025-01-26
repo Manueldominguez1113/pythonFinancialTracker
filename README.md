bfe791f496f8ae14ba41e35431aa5bf80d13124a
manuel <manuel.dominguez1113@gmail.com>
2025-01-25 20:23:44 -0500

    completed a majority of the functions

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 private financial tracker to help me keep up with everything
 also an excuse to make something in python. 
 also an excuse to use pandas to read/write into an xlsx file.
+
+currently, all my normal functions have been made. 
+I plan to next:
+- create a gui to create an application. showing all three debts, bills, and incomes. maybe in different tabs
+- allow editing the text, so that i can make update/add/del buttons.
+- attach functions to buttons
+- detach my example fincancial xlsx file, allowing the app to create a blank and fill it up with user info
--- financialtracker.py
+++ financialtracker.py
@@ -1,19 +1,30 @@
 import pandas as pd
 
 xlsx = pd.ExcelFile("financials.xlsx")
+
 debts = pd.read_excel(xlsx, "debts")
 bills = pd.read_excel(xlsx, "bills")
 income = pd.read_excel(xlsx, "income")
-total_debts = round(debts["owe"].sum(), 2)
 
+def sheet_info(sheet):
+    items =[]
+    for row in sheet.iterrows():
+        items.append(row)
+    return items
 
 # debt functions...
-def __calc_paid(row):
-    name = row.loc["credit card name"]
-    owe = row.loc["owe"]
+def get_row_info(row):
+    name = row.loc["name"]
+    owe = row.loc["bal"]
     apr = row.loc["apr"]
     mpr = apr / 12
     fee = row.loc["monthly fee"]
+
+    return name, owe, apr, mpr, fee
+
+def __calc_paid(row):
+    name, owe, apr, mpr, fee = get_row_info(row)
+
     months = 0
     if owe * mpr > fee:
         raise Exception(f"{name}'s interest({apr}) is higher than fee({fee}), will never pay off at this rate!!")
@@ -51,38 +62,143 @@ def calc_till_paid(row):
         i += 1
 
 
-    # leftover debt functions
-    # need a simulate function where itll render next months totals as if i paid this month an amount ill use as an arg,but not save the totals
-    #     use yield to mget the next month and wait in case i want to simulate the month after.
-    #     make it say "after x months, this debt will look like {stats}"
+    # TODO:need a simulate function where ill use as an arg for a custom what if payment,but not save the totals
 
 
-    # bills functions...
-    # need to total the bills. sum method.
-    # if recurring, add a bool for future simulate function
+def update_debt_month(update=False):
+    bals = debts["bal"].tolist()
+    aprs = debts["apr"].tolist()
+    payments = debts["monthly fee"].tolist()
 
-    # need an actual function where a month has passed and i need it to update the totals( debts and bills func)
+    while any(bal >0 for bal in bals):
+        totals = []
+        for bal, apr, payment in zip(bals, aprs, payments):
+            if bal > 0:
+                if apr == 0:
+                    next_bal = bal - payment
+                else:
+                    next_bal = bal + (bal * (apr/12)) - payment
 
-    # income functions...
-    # need to total,
-    # thats about it for now..
+                next_bal = round(max(next_bal, 0),2)
 
-    # total functions..
-    # need to show all totals.
-    # net loss / net gain per month(per year?)
-    # how much i would need to make to sustain. add an overload in case i want to simulate groceries or other stuff.
-    # add edit delete for all
+                totals.append(next_bal)
+            else:
+                totals.append(0)
+
+        if update:
+            debts["bal"] = totals
+            return
+
+        bals = totals
+        yield bals
+
+def update_row_by_month(row, fee=None):
+    if fee is None:
+        _,owe,_,mpr,fee = get_row_info(row)
+    else:
+        _,owe,_,mpr,_ = get_row_info(row)
+
+    next_bal = owe + (owe * mpr) - fee
+
+    return next_bal
 
 
-def printall():
-    calc_till_paid(debts)
-    #     # print(self.debts)
-    #     # print("\n---------------\n")
-    #     # print(self.bills)
-    #     # print("\n---------------\n")
-    #     # print(self.income)
-    #     # print("\n---------------\n")
-    #     # print(self.total_debts)
+def total_debts():
+    return round(debts["bal"].sum(), 2)
+
+def total_debt_pay():
+    return round(debts["monthly fee"].sum(), 2)
+
+# bills functions...
+def total_bills():
+    return round(bills["amount"].sum(),2)
+
+# income functions...
+def total_income():
+    return round(income["amount"].sum(),2)
+
+# total functions.
+def total_all():
+    tot_debt = total_debts()
+    tot_bills = total_bills()
+    tot_income = total_income()
+
+    return tot_debt, tot_bills, tot_income
+
+def net_change(debt =None, bill=None, inc=None,extra = 0, vers="net"):
+    """
+    shows net worth. takes in three versions(vers): net, month, or annual, to calculate the respective worth
+
+    :param debt:
+    :param bill:
+    :param inc:
+    :param extra:
+    :param vers:
+    :return:
+
+    """
+    if debt is None:
+        if vers == "net":
+            debt = total_debts()
+        elif vers == "month":
+            debt = total_debt_pay()
+    if bill is None:
+        bill = total_bills()
+    if inc is None:
+        inc = total_income()
+
+    if vers == "annual":
+        bill *= 12
+        debt *= 12
+        inc *= 12
+        extra *= 12
+
+    return (debt + bill+ extra) - inc
 
 
-print(printall())
+    # how much i would need to make to sustain.(can be a button)
+
+def add_cell_debts(name, owe, apr, mpr, fee):
+    new_row = pd.DataFrame([[name, owe, apr, mpr, fee]])
+    debts.concat(new_row, ignore_index=True)
+
+def add_cell_bills(name, amount, due):
+    new_row = pd.DataFrame([[name, amount, due]])
+    bills.concat(new_row, ignore_index=True)
+
+def add_cell_income(name, amount, date):
+    new_row = pd.DataFrame([[name, amount, date]])
+    income.concat(new_row, ignore_index=True)
+
+def edit_cell(sheet, itemname, valuetype, new_value):
+    update = sheet[sheet["name"] == itemname]
+    if not update.empty:
+        sheet.loc[update, valuetype] = new_value
+    else:
+        print(f"could not find {itemname}, {valuetype}, in {sheet}")
+
+    return sheet
+
+def delete_cell(sheet, index):
+    sheet.delete_row(index)
+
+
+
+
+
+
+
+
+
+
+# update = update_debt_month(debts)
+
+# month_1 = next(update)
+# print(f"month 1 : {month_1}")
+# month_2 = next(update)
+# print(f"month 2 : {month_2}")
+
+# for month, bals in enumerate(update):
+#     print(f"month {month+1}: {bals}")
+# print(sheet_info(debts))
+print(total_all())
